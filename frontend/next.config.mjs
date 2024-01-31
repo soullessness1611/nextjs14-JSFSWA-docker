7bc71e150a379975d99fe6244d517e48429dc551
soullessness1611 <patiwatnakara@gmail.com>
2024-02-01 02:21:26 +0700

    init commit

---
+++ frontend/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+//
+// export default nextConfig;
+
+const nextConfig = {
+  output: "standalone",
+};
+
+export default nextConfig;
